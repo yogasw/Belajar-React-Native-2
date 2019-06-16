b55629e5b1e28cb4e66e50e2252332dc4c367e32
Yoga Setiawan <mryoga1995@gmail.com>
2019-06-16 17:31:43 +0700

    Flex Dimension

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
-
-
-
-
-
-
-
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, StatusBar,Platform, Text, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Ini Button</Text>
-      <View style={{width:50, height:50, backgroundColor:'powderblue'}} />
-      <View style={{width:100, height:100, backgroundColor:'skyblue'}}/>
-      <View style={{width:150, height:150, backgroundColor:'black'}}/>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.ini_button}>Ini Button</Text>
+
+            <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+
+            <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+
+            <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    ini_button: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 20,
+        textAlign:'center'
+    }
 });
